736af9ae56b8a7cfbe56e772039ce48d7c9fd8bc
GuillotJessica <babaorhum001@gmail.com>
2017-08-24 18:01:22 +0200

    MeanStackHerokuAuthenticationApplication

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 const users = require('./routes/users');
 
 // Port Number
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 // CORS Middleware
 app.use(cors());
@@ -46,6 +46,10 @@ app.use('/users', users);
 // Index Route
 app.get('/', (req, res) => {
   res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start Server
