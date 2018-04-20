b4c2e8e3e64b636510d47c48823b70096c0e6c28
Jessica G <guillotjessicab@gmail.com>
2018-04-20 11:36:04 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
+//HTTP utility methods and middleware at your disposal, creating a robust API is quick and easy. 
 const express = require('express');
 const router = express.Router();
+//Passport is authentication middleware for Node.
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
